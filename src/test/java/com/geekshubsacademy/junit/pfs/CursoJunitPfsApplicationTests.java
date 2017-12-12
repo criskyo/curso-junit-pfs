a9778a892c676d12ddbdd4747947f09ba0d829db
mefernandez <mariano.fernandez@gmail.com>
2017-12-12 13:02:44 +0100

    Comentario sobre @Category y JUnit 4.4

--- src/test/java/com/geekshubsacademy/junit/pfs/CursoJunitPfsApplicationTests.java
+++ src/test/java/com/geekshubsacademy/junit/pfs/CursoJunitPfsApplicationTests.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+// @Category no funciona con JUnit 4.4
+// JUnit 4.4 es necesario para spring-test:2.5.6.SEC01 
 //@Category(TestDeIntegracion.class)
 @ContextConfiguration(locations = "classpath:spring-context.xml")
 public class CursoJunitPfsApplicationTests {
